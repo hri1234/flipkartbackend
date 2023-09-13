de8b4595a1e107b124f4ee55744056b58c06e463
hritik pandey <128885231+hri1234@users.noreply.github.com>
2023-09-13 17:18:09 +0530

    Update database.js
    4th commit

--- backend/config/database.js
+++ backend/config/database.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 const MONGO_URI = process.env.MONGO_URI;
 
 const connectDatabase = () => {
-    mongoose.connect(MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+    mongoose.connect('mongodb+srv://hrithik:io9nn9UObqOcARiI@cluster0.fe4kclq.mongodb.net/ecomm', { useNewUrlParser: true, useUnifiedTopology: true })
         .then(() => {
             console.log("Mongoose Connected");
         });
